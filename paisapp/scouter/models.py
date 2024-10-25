baae41cad56bb75adf0651b46771b1decd576859
Adi <asriram3@illinois.edu>
2024-10-24 18:34:32 -0600

    Scout name allowed to be blank

--- paisapp/scouter/models.py
+++ paisapp/scouter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Scout(models.Model):
-	name = models.CharField(max_length=255)
+	name = models.CharField(max_length=255, blank=True)
 	img = models.ImageField(upload_to='scouts/')
 	latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
 	longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
